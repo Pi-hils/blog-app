6f69485de941da347e2876e3fe3d70f8bd2937bd
Hilda Amponsah <hilda.amp@hotmail.com>
2021-05-22 14:55:31 +0100

    added paths of each component to app-routing-module.ts

--- blog-app/src/app/app-routing.module.ts
+++ blog-app/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PostComponent } from './post/post.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  {path: "login", component: LoginComponent},
+  {path: "post", component: PostComponent},
+  {path: "register", component: RegisterComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
